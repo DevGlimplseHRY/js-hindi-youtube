d07b1ac6013de677bd480f7a18bb84befe472867
DevGlimplseHRY <155462203+DevGlimplseHRY@users.noreply.github.com>
2024-01-28 17:03:19 +0000

    Add some tasks inside comments

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,17 @@
-console.log("Ishab")
+// console.log("Ishab")
+
+var accountUser = "Harish"
+console.log(accountUser)
+// {
+//     accountUser = "Sahil"
+//     console.log(accountUser)
+// }
+// console.log(accountUser)
+
+
+/* 
+    Declare a variable with let keyword
+    And then Make a function & change that variable's value inside the function
+    then check out whether & where where variable's value changes 
+    Then come on the conclusion of why var is not used bcz of issue in block/functional scope
+*/
