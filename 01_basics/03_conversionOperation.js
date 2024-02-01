f8f656ac316644bc7abede211bf8acd51a654784
DevGlimplseHRY <155462203+DevGlimplseHRY@users.noreply.github.com>
2024-02-01 07:51:04 +0000

    COnversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+// let score = "33abc"
+// let score = null
+// let score = undefined
+
+console.log(score)
+console.log(typeof score)
+console.log(typeof (score))
+
+console.log(score)
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber))
+console.log(valueInNumber)
+
+let score = 0
+let booleanValue = Boolean(score)
+
+console.log (typeof booleanValue)
+console.log(booleanValue)
+
+
+// "33" => 33
+// "33abc" => NaN
+                        //  --typeof NaN => Number
+// true => 1 ; false => 0
+// "" => false
+// "hitesh" => true
