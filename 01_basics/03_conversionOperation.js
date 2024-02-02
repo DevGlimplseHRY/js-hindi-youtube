f72c1f4bceba0794b518208c5f926b5f622ed0bf
DevGlimplseHRY <155462203+DevGlimplseHRY@users.noreply.github.com>
2024-02-02 12:35:18 +0000

    Add some OPERATIONS

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,26 +1,53 @@
-// let score = "33abc"
+// // let score = "33abc"
 // let score = null
-// let score = undefined
+// // let score = undefined
 
-console.log(score)
-console.log(typeof score)
-console.log(typeof (score))
+// // console.log(score)
+// console.log(typeof score)
+// // console.log(typeof (score))
 
-console.log(score)
-let valueInNumber = Number(score)
-console.log(typeof (valueInNumber))
-console.log(valueInNumber)
+// // console.log(score)
+// // let valueInNumber = Number(score)
+// // console.log(typeof (valueInNumber))
+// // console.log(valueInNumber)
 
-let score = 0
-let booleanValue = Boolean(score)
+// // let score = 0
+// // let booleanValue = Boolean(score)
 
-console.log (typeof booleanValue)
-console.log(booleanValue)
+// // console.log (typeof booleanValue)
+// // console.log(booleanValue)
 
 
-// "33" => 33
-// "33abc" => NaN
-                        //  --typeof NaN => Number
-// true => 1 ; false => 0
-// "" => false
-// "hitesh" => true
+// // "33" => 33
+// // "33abc" => NaN
+//                         //  --typeof NaN => Number
+// // true => 1 ; false => 0
+// // "" => false
+// // "hitesh" => true
+
+
+// // **************************** Operations ******************************** //
+
+// // let value = 3 ; 
+// // let negValue = -value ;
+// // console.log(negValue);
+
+// // console.log(2 + 2)
+// // console.log(2 / 2)
+// // console.log(2 % 2)
+
+// // let string1 = "Hello" ;
+// // let string2 = " Ishab";
+// // let string3 = string1 + string2 ;
+
+// // console.log(string3);
+
+// // console.log(1 + "2");
+// // console.log("1" + 2);
+// // console.log("1" + 2 + 2);
+// // console.log(1 + 2 + "2");
+
+// console.log(true);
+// console.log(+true);
+// // console.log(true+);
+// console.log(+"");
