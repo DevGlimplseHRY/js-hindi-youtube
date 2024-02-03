82678bc7c6422da3eea47460d95c2e6149df23f8
DevGlimpseHRY <ishabdhillon1@gmail.com>
2024-02-03 21:52:32 +0530

    dataType summary

---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,31 @@
+//  Primitive 
+
+// 7 types : String , Number , Boolean , null , undefined , Symbol , BigInt
+
+
+// Reference (Non Primitive)
+
+// Arrays , Objects , Functions
+
+
+
+
+  
+// const id = Symbol("134")
+// const anotherId = Symbol("134");
+// console.log(id === anotherId)
+
+//const bigNumber = 34343435634664n
+
+// const heros = ["shaktiman" , "naagraj" , "doga"] ;
+// let myobj = {
+//     name : "Ishab",
+//     age : 21
+// }
+
+// const myFunction = function () {
+//     console.log("Hello World !")
+//     "Hello Word"
+// }
+// console.log(typeof myFunction)               // --   typeof specifies [return]/result type 
+// console.log(myFunction)
