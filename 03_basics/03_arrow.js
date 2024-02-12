1c3c61d8efb48e41a3818cfa64ac32a651c5d8a2
DevGlimpseHRY <ishabdhillon1@gmail.com>
2024-02-12 18:49:25 +0530

    Learn about => functions & this keyword .

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,54 @@
+const user = {
+    username : "ishab",
+    price    : 999,
+
+    welcomeMessage : function () {
+        console.log(`${this.username} , welcome to the website`)
+        console.log(this);
+    }
+}
+user.welcomeMessage();
+user.username = "Sam";
+user.welcomeMessage();
+
+console.log(this);
+
+function chai(){
+    let username = "hitesh"
+    // console.log(this.username);      // can't use this keyword in functions
+    console.log(username);
+}
+chai();
+
+
+const chai = function () {
+//     let username = "Harry";
+//     // console.log(this.username);
+    console.log(this);
+//     console.log(username);
+}
+chai();
+
+const chai = () => {
+    let username = "ishab";
+    // console.log(this.username);
+    // console.log(this);       // this keyword in arrow function gives empty object whereas traditional/classic/regular
+                                //  function gives so many values .
+    console.log(username);
+}
+chai();
+
+const addTwo = (num1,num2) => {
+    return (num1 + num2);
+}
+console.log(addTwo(3,4));
+
+// ANOTHER WAY TO WRITE ARROW FUNCTION
+
+// const addTwo = (num1 , num2) => num1 + num2 ;
+
+// const addTwo = (num1 , num2) => (num1 + num2);
+
+// const addTwo = (num1 , num2) => ({username : "ishab"})
+
+console.log(addTwo(3,4));
