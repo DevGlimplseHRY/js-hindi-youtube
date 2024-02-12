336b48370d425b10dec4e3b7f547068c89e74ff0
DevGlimplseHRY <155462203+DevGlimplseHRY@users.noreply.github.com>
2024-02-12 19:03:20 +0530

    just refactor the file

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,52 +1,55 @@
-// // global scope
-// var c = 300
-// let a = 12
-// if(true){
-//     // block scope
-//     let a = 10 ;
-//     const b = 20 ;
-//     // var c = 30 ;
-//     // console.log(a);
-// }
-// // console.log(a);
-// // console.log(b);
-// // console.log(c);
+// global scope
+var c = 300
+let a = 12
 
-// function one (){
-//     const username = "Hitesh";
-//     function Two(){
-//         const website = "youtube"
-//         // console.log(username);
-//     }
+if (true) {
+    // block scope
 
-//     // console.log(website);
+    let a = 10;
+    const b = 20;
+    // var c = 30 ;
+    console.log(a);
+}
 
-//     Two()
-// }
-// one();
+console.log(a);
+console.log(b);
+// console.log(c);
+
+function one() {
+    const username = "Hitesh";
+    function Two() {
+        const website = "youtube"
+        console.log(username);
+    }
+
+    console.log(website);
+
+    Two()
+}
+one();
 
 
-// if(true){
-//     const username = "ishab"
-//     if(username === "ishab"){
-//         const website = " facebook"
-//         console.log(username + website);
-//     }
-//     // console.log(website);
-// }
-// // console.log(username);
+if (true) {
+    const username = "ishab"
+    if (username === "ishab") {
+        const website = " facebook"
+        console.log(username + website);
+    }
+    console.log(website);
+}
+console.log(username);
 
 
 // +++++++++++++++++++   interesting   ++++++++++++++++++++++ //
 
 console.log(addOne(5));
-function addOne(num){
+function addOne(num) {
     return (num + 1)
 }
 
 
 // console.log(addTwo(5));          // cannot use variable assigned function before initialization
-const addTwo = function (num){
+const addTwo = function (num) {
     return (num + 2);
 }
 
