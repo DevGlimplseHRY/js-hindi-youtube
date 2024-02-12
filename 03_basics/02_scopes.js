66812bb99ab7bffab3bb7276c5387a9762e28529
DevGlimpseHRY <ishabdhillon1@gmail.com>
2024-02-12 18:42:50 +0530

    Learn about scope & mini-hoisting .

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,53 @@
+// // global scope
+// var c = 300
+// let a = 12
+// if(true){
+//     // block scope
+//     let a = 10 ;
+//     const b = 20 ;
+//     // var c = 30 ;
+//     // console.log(a);
+// }
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
+
+// function one (){
+//     const username = "Hitesh";
+//     function Two(){
+//         const website = "youtube"
+//         // console.log(username);
+//     }
+
+//     // console.log(website);
+
+//     Two()
+// }
+// one();
+
+
+// if(true){
+//     const username = "ishab"
+//     if(username === "ishab"){
+//         const website = " facebook"
+//         console.log(username + website);
+//     }
+//     // console.log(website);
+// }
+// // console.log(username);
+
+
+// +++++++++++++++++++   interesting   ++++++++++++++++++++++ //
+
+console.log(addOne(5));
+function addOne(num){
+    return (num + 1)
+}
+
+
+// console.log(addTwo(5));          // cannot use variable assigned function before initialization
+const addTwo = function (num){
+    return (num + 2);
+}
+
+console.log(addTwo(5));
