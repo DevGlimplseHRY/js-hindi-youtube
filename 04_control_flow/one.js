e343124c9c004374a690bae2fae7136017cdae66
DevGlimpseHRY <ishabdhillon1@gmail.com>
2024-02-14 13:51:40 +0530

    control flow code files

---
+++ 04_control_flow/one.js
@@ -0,0 +1,45 @@
+const isUserLoggedIn = false ;
+const temperature = 41 ;
+// if(2 === "2"){
+//     console.log("executed")
+// }
+// if(temperature <= 50){
+//     console.log(`Temperature is less than 50 .`);
+// }else{
+//     console.log(`Here temperature is greater than 50.`);
+// }
+
+// <, >, <=, >=, ==, ===, !=, !== 
+
+
+// const score = 200 ;
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`user power : ${power}`);
+// }
+// console.log(`user power : ${power}`);
+
+// const balance = 1000 ; 
+// if(balance > 500) console.log("test") , console.log("test2");
+// if(balance < 500) {
+//     console.log("less than 500");
+// }else if(balance < 750){
+//     console.log("less than 750");
+// }else if(balance < 900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true ;
+const debitCard = true ; 
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true ;
+
+if(userLoggedIn && debitCard){
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged in");
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,44 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3 ;
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("default value executed");
+        break;
+}
+
+
+// const balance = 500 ;
+// switch(balance){
+//     case 500 : 
+//         console.log("balance is less than 500");
+//     break;
+//     case 750 : if(balance < 750){
+//         console.log("balance is less than 750");
+//         break;
+//     }
+//     default :
+//     console.log("wrong input");
+//     break;
+// }
