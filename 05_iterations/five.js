9ff2a2248a4da59984548a83107424ad44893f15
DevGlimpseHRY <ishabdhillon1@gmail.com>
2024-02-16 23:50:39 +0530

    learn about for-each property/loop through which we can access objects & their properties in arrays.

---
+++ 05_iterations/five.js
@@ -0,0 +1,40 @@
+const coding = ["js" , "ruby" , "java" , "python" , "cpp"]
+
+// coding.forEach( function (val){         // this () is known as callback function 
+//     console.log(val)
+// })
+
+// console.log();
+
+// coding.forEach( (item) => {
+//     console.log(item)
+// }) 
+
+// function printMe (item){
+//     console.log(item)
+// }
+
+// coding.forEach(printMe);        // just pass reference of function , not execute the function
+
+// coding.forEach( (item , index , arr) => {
+//     console.log(item, index, arr)
+// })
+
+const myCoding = [
+    {
+        languageName : "javaScript",
+        languageFileName : "js"
+    },
+    {
+        languageName : "java",
+        languageFileName : ".java"
+    },
+    {
+        languageName : "python",
+        languageFileName : "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageFileName);
+})
