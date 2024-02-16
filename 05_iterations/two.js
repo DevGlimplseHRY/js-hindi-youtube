e29421ebacf4f0b0262416d1b0c1f25d720d70df
DevGlimpseHRY <ishabdhillon1@gmail.com>
2024-02-16 16:36:31 +0530

    iteration using while & do-while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,37 @@
+// let i = 0 ;
+
+// const myArray = ['flash','batman','superman'];
+// // while (i <= 10) {
+// //     console.log(i);
+// //     i++;
+// // }
+// console.log(myArray.length)
+// while(myArray[i] <= myArray.length){
+//     console.log(myArray[i]);
+//     i++;
+// }
+// // do {
+    
+// // } while (condition);
+
+
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of index is : ${index}`);
+    index += 2 ;
+}
+
+let myArray = ['flash','batman','superman'];
+
+let arr = 0 ;
+while (arr < myArray.length){
+    // console.log(`Value is ${myArray[arr]}`)
+    arr++ ;
+}
+
+
+let score = 11 ;
+do {
+    console.log(`Score is : ${score}`)
+    score ++ ;
+} while (score <= 10);
