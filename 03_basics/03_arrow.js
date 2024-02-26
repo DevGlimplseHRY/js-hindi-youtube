c1ae900361b5b6d224e344f927b3e740bcf91e36
DevGlimpseHRY <ishabdhillon1@gmail.com>
2024-02-26 20:11:34 +0530

    Just updated

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -13,42 +13,42 @@ user.welcomeMessage();
 
 console.log(this);
 
-function chai(){
-    let username = "hitesh"
-    // console.log(this.username);      // can't use this keyword in functions
-    console.log(username);
-}
-chai();
+// function chai(){
+//     let username = "hitesh"
+//     // console.log(this.username);      // can't use this keyword in functions
+//     console.log(username);
+// }
+// chai();
 
-
-const chai = function () {
+// i just comment rest of the following code from here today .
+// const chai = function () {
 //     let username = "Harry";
 //     // console.log(this.username);
-    console.log(this);
+//     // console.log(this);
 //     console.log(username);
-}
-chai();
+// }
+// chai();
 
-const chai = () => {
-    let username = "ishab";
-    // console.log(this.username);
-    // console.log(this);       // this keyword in arrow function gives empty object whereas traditional/classic/regular
-                                //  function gives so many values .
-    console.log(username);
-}
-chai();
+// const chai = () => {
+//     let username = "ishab";
+//     console.log(this.username);
+//     // console.log(this);       // this keyword in arrow function gives empty object whereas traditional/classic/regular
+//                                 //  function gives so many values .
+//     console.log(username);
+// }
+// chai();
 
-const addTwo = (num1,num2) => {
-    return (num1 + num2);
-}
-console.log(addTwo(3,4));
+// const addTwo = (num1,num2) => {
+//     return (num1 + num2);
+// }
+// console.log(addTwo(3,4));
 
-// ANOTHER WAY TO WRITE ARROW FUNCTION
+// // ANOTHER WAY TO WRITE ARROW FUNCTION
 
-// const addTwo = (num1 , num2) => num1 + num2 ;
+// // const addTwo = (num1 , num2) => num1 + num2 ;
 
-// const addTwo = (num1 , num2) => (num1 + num2);
+// // const addTwo = (num1 , num2) => (num1 + num2);
 
-// const addTwo = (num1 , num2) => ({username : "ishab"})
+// // const addTwo = (num1 , num2) => ({username : "ishab"})
 
-console.log(addTwo(3,4));
+// console.log(addTwo(3,4));
